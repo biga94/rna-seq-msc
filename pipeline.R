5dc70b7d59a0e80a671980678c1a8780f0c19fe4
Davide Biganzoli <92393900+biga94@users.noreply.github.com>
2023-02-22 12:01:53 +0100

    Update pipeline.R
    tidying script

--- pipeline.R
+++ pipeline.R
@@ -1,50 +1,44 @@
-## ----Install edgeR, message=FALSE------------------------------------------------------------------------------------------------------------------------------------
-#BiocManager::install("edgeR")
-
-
-## ----Select edgeR----------------------------------------------------------------------------------------------------------------------------------------------------
 library(edgeR)
 
 
-## ----Data Load-------------------------------------------------------------------------------------------------------------------------------------------------------
-setwd("~/Documents/BIOTECHIE/Unimi/Methods in Bioinformatics/info18/files_method_bioinformatics_exam")
+#Loading data
+#setwd("~/Documents/BIOTECHIE/Unimi/Methods in Bioinformatics/info18/files_method_bioinformatics_exam")
 raw_counts <- read.csv("data.csv", header = T,  sep = "\t", row.names = 1) 
 group <- read.csv("conditions.csv", header = T, sep = "\t", row.names = 1)
 #row.names=1 because the objects of the first column give the names to the different rows of the data frame
 #header= TRUE because the objects of the first row give the names to the different columns of the data frame
 
 
-## ----Order-----------------------------------------------------------------------------------------------------------------------------------------------------------
+# Order
 #with this line of code we create a dataframe which will have as columns names the names of the rows in the data.frame 'group' in the SAME order
 counts <- raw_counts[,rownames(group)] 
 #this instead is a logical vector that demonstrate that what we have done is correct
 colnames(counts) == rownames(group)
 
 
-## ----DGEList---------------------------------------------------------------------------------------------------------------------------------------------------------
+# DGEList
 #we set that the library size of each replicate (counts columns) is equal to the sum of each value whithin the column i.e. the reads of each gene for each condition
 countsDGE <- DGEList(counts= counts, group = group$tissue, remove.zeros = T, lib.size =colSums(counts))
 
 
-## ----Filter----------------------------------------------------------------------------------------------------------------------------------------------------------
+# Filter
 #we have summarized this operation in one string of code
 countsDGE$counts <- countsDGE$counts[apply(countsDGE$counts>10,1,sum)>0,]
 
 
-## ----TMM-------------------------------------------------------------------------------------------------------------------------------------------------------------
+# TMM
 #this will add a column named "norm.factors" ro the samples data.frame of the DEGList
 N.countsDGE <- calcNormFactors(countsDGE, method = "TMM")
 
 
-## ----MDS-------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+# MDS
 DiffCol <- c("red","orange","blue","green","mediumorchid1","black")[N.countsDGE$samples$group]
 plotMDS(N.countsDGE, col=DiffCol, pch = 19)
 legend("topright",fill=c("red","orange","blue","green","mediumorchid1","black"),legend=levels(N.countsDGE$samples$group))
 
 
 
-## ----NB Fitting------------------------------------------------------------------------------------------------------------------------------------------------------
+# NB Fitting
 #if not specified the type of dispersion used will be tagwise
 D.countsDGE <- estimateDisp(N.countsDGE)
 #by using exactTest an object DGEExact is returned
@@ -52,12 +46,12 @@ eT <- exactTest(D.countsDGE, pair = c("Frontal Cortex (BA9)","Substantia nigra")
 
 
 
-## ----not Filtered DEGs-----------------------------------------------------------------------------------------------------------------------------------------------
+# not Filtered DEGs
 Ctt <- topTags(eT, n=nrow(eT)) 
 #these are the genes that are presumably DE but that are not yet filtered for an FDR
 
 
-## ----Filtered Degs---------------------------------------------------------------------------------------------------------------------------------------------------
+# Filtered Degs
 #real DEGs have an FDR lower than 0.01
 DE_UP <- (Ctt[Ctt$table$FDR <= 0.01 & Ctt$table$logFC>0,])
 DE_DOWN <- (Ctt[Ctt$table$FDR <= 0.01 & Ctt$table$logFC<0,])
@@ -66,7 +60,7 @@ notDE_UP <- (Ctt[Ctt$table$FDR > 0.01 & Ctt$table$logFC>0,])
 notDE_DOWN <- (Ctt[Ctt$table$FDR > 0.01 & Ctt$table$logFC<0,])
 
 
-## ----DEGs Boxplots---------------------------------------------------------------------------------------------------------------------------------------------------
+# DEGs Boxplots
 par(mfrow=c(2,2))#to visualize the boxplots in a 2x2 grid
 boxplot(DE_UP$table$logFC, main = "logFC DE_UP", outline = F, col = 'mediumorchid')
 boxplot(DE_DOWN$table$logFC, main = "logFC DE_DOWN", outline = F, col = 'mediumorchid2')
@@ -74,35 +68,34 @@ boxplot(notDE_UP$table$logFC, main = "logFC notDE_UP", outline = F, col = 'mediu
 boxplot(notDE_DOWN$table$logFC, main = "logFC notDE_DOWN", outline = F, col = 'violet')
 
 
-## ----S.Nigra Fr. Cortex filter---------------------------------------------------------------------------------------------------------------------------------------
+# S.Nigra Fr. Cortex filter
 #first we filter the conditions of the replicates for which neither Frontal Cortex nor Substantia Nigra is sampled
 group2 <- group[-c(1:12,19:30),] 
 #then we filter out gene counts that are referring only to the replicates for which our tissues are sampled by indexing the columns of the columns of the counts data.frame
 counts2 <- counts[,rownames(group2)]
 
 
-## ----Subsetting Fr. Cortex-------------------------------------------------------------------------------------------------------------------------------------------
+# Subsetting Fr. Cortex
 #Subsetting Frontal Cortex
 groupFrC <- group2[-c(7:12),]
 countsFrC <- counts2[,rownames(groupFrC)]
 
 
 
-## ----Filtering-------------------------------------------------------------------------------------------------------------------------------------------------------
+# Filtering
 #we need to set group = groupFrC$sex to add to the DEGList object a column in the samples data.frame that assigns the sex membership of each sample
 DGEFrC <- DGEList(counts = countsFrC, group = groupFrC$sex)
 DGEFrC$counts <- DGEFrC$counts[apply(DGEFrC$counts>10,1,sum)>1,]
 
 
-## ----Normalization---------------------------------------------------------------------------------------------------------------------------------------------------
+# Normalization
 #normalization
 DGEFrC <- calcNormFactors(DGEFrC)
 #pseudocounts are essential to deal with 0s
 logDGEFrC <- log(DGEFrC$counts + 0.01)
 
 
-## ----PCA-------------------------------------------------------------------------------------------------------------------------------------------------------------
-#PCA 
+# PCA 
 col.sex <- c("pink","lightblue")[DGEFrC$samples$group]
 #it is essential to apply the prcomp() function on the columns of the data.frame containing the counts because the columns contain the id of each replicate; i need to compare the replicates, not the genes
 PCAFrC <- prcomp(t(logDGEFrC))
@@ -110,15 +103,15 @@ plot(PCAFrC$x, col = col.sex, pch = 19, main = "Frontal Cortex PCA")
 legend("bottomright",fill = c("pink","lightblue"), legend = levels(DGEFrC$samples$group))
 
 
-## ----NB Fitting Fr. Cortex-------------------------------------------------------------------------------------------------------------------------------------------
+# NB Fitting Fr. Cortex
 D.DGEFrC <- estimateDisp(DGEFrC)
 
 
-## ----DEGs------------------------------------------------------------------------------------------------------------------------------------------------------------
+# DEGs
 eTFrC <- exactTest(D.DGEFrC, pair = c(1,2))
 
 
-## ----filtered DEGs---------------------------------------------------------------------------------------------------------------------------------------------------
+# filtered DEGs
 FrCTT <- topTags(eTFrC, n=nrow(eTFrC))
 #in task 7 we filtered data both for logFC and for FDR, here we filter only for an FRD
 genes_sex_spec <- FrCTT[FrCTT$table$FDR <= 0.05,]
@@ -126,15 +119,14 @@ genes_sex_spec <- FrCTT[FrCTT$table$FDR <= 0.05,]
 head(genes_sex_spec)
 
 
-## ----used genes legend-----------------------------------------------------------------------------------------------------------------------------------------------
+# used genes legend
 library(biomaRt)
 ensembl = useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
 genes_used <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"), filters = "ensembl_gene_id",values = rownames(genes_sex_spec), mart = ensembl)
 genes_used
 
 
-## ----all about Substantia Nigra--------------------------------------------------------------------------------------------------------------------------------------
-
+# all about Substantia Nigra
 #Subsetting Substantia Nigra
 
 groupSubNig <- group2[c(7:12),]
@@ -182,7 +174,7 @@ genes_used2 <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"), filters =
 genes_used2
 
 
-## ----Venn for common sex---------------------------------------------------------------------------------------------------------------------------------------------
+# Venn for common sex
 library('VennDiagram')
 Frc_string <- rownames(genes_sex_spec)
 SubNig_string <- rownames(genes_sex_spec2)
@@ -190,7 +182,7 @@ listalo <- list(Frc_string, SubNig_string)
 venn.diagram(listalo, category.names = c("F.Cortex", "S.Nigra"),filename = "DEGsex.png", imagetype = "png",main = "Sex specific genes in Frontal Cortex and Substantia Nigra", col = c("mediumorchid", "mediumorchid4"))
 
 
-## ----Venn DEGs and sex DEGs------------------------------------------------------------------------------------------------------------------------------------------
+# Venn DEGs and sex DEGs
 #VENN DIAGRAM between DEGs in common part (common part), S.Nigra and Fr. Cortex
 Ctt_string <- rownames(Ctt[Ctt$table$FDR <= 0.01,])
 Camillibus <- list(Frc_string, SubNig_string, Ctt_string)
